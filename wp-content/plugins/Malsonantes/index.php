5c834297c16ea9af7c4832622045c2138c417878
Alejandro Pereiro G <jpereirogabian@danielcastelao.org>
2022-11-20 23:50:57 +0100

    Agregado el insert en la tabla y el cambio de las palabras con esta

--- wp-content/plugins/Malsonantes/index.php
+++ wp-content/plugins/Malsonantes/index.php
@@ -10,7 +10,16 @@ Version: 1.0
  */
 function malsonantes( $text ) {
     $malas = array('malo', 'feo', 'tonto', 'idiota');
-    $buenas = array('bueno', 'lindo', 'listo', 'inteligente');
+
+    //Objeto para trabajar con la bd
+    global $wpdb;
+
+    //Buscamos la tabla por el nombre
+    $mitabla = $wpdb -> prefix."malsonantes";
+
+    //Buscamos las palabras que esten relacionadas
+    //si no pasamos variables no hay que usar prepare
+    $buenas = $wpdb-> get_results($wpdb -> prepare("select * from $mitabla where mal = ($malas)"));
     return str_replace( $malas, $buenas, $text );
 }
 
@@ -30,9 +39,10 @@ function myplugin_bd_table() {
     $table_name = $wpdb->prefix . 'malsonantes';
 
     //indicamos la sentencia
-    $sql = "CREATE TABLE $table_name (
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
         mal varchar(20),
-        bien varchar(20)
+        bien varchar(20),
+        primary key mal
     ) $charset_collate;";
 
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
@@ -43,3 +53,33 @@ function myplugin_bd_table() {
  * Ejecuta el plugin cuando este se carga
  */
 add_action( 'plugins_loaded', 'myplugin_bd_table' );
+
+/*
+ * Añade el contenido a la tabla anteriormente creada
+ */
+function myplugin_bd_content() {
+
+    //Objeto para trabajar con la bd
+    global $wpdb;
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    //Indicamos el nombre de nuestra tabla
+    $table_name = $wpdb->prefix . 'malsonantes';
+
+    //Creamos las dos arrays de los elementos que vamos a querer introducir en nuestra tabla
+    $malas = array('malo', 'feo', 'tonto', 'idiota');
+    $buenas = array('bueno', 'lindo', 'listo', 'inteligente');
+
+    //indicamos la sentencia
+    $sql = $wpdb -> prepare("INSERT INTO $table_name (mal, bien) VALUES ($malas, $buenas)") ;
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+/*
+ * Ejecuta el plugin cuando este se carga
+ */
+add_action( 'plugins_loaded', 'myplugin_bd_content' );
+
